e48036085dbb9a1e2d91b4f7c492fcfbc87d480d
Brian Harianja <shidoitsukaa@gmail.com>
2020-10-07 01:40:01 +0700

    working high score!

--- script.js
+++ script.js
@@ -3,5 +3,8 @@
   if (localStorage.getItem("highscore") != null) {
     document.querySelector(".high-score-value").innerHTML = localStorage.getItem("highscore");
   }
-  else localStorage.setItem("highscore", 0);
+  else {
+    localStorage.setItem("highscore", 0);
+    document.querySelector(".high-score-value").innerHTML = localStorage.getItem("highscore");
+  }
 })();
